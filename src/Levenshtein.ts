bd944fb099d55717cb7da25a4e3eaff19b455048
blupblup46 <louan46@gmail.com>
2024-04-02 10:17:13 +0200

    rework levenssqudghqsf

--- src/Levenshtein.ts
+++ src/Levenshtein.ts
@@ -3,21 +3,42 @@ function minimum(nb1: number, nb2: number, nb3: number): number {
 }
 
 
-export function levenshtein(str1: string, str2: string){
+export function levenshtein(str1: string, str2: string) {
 
     let i: number;
     let j: number;
-    let cost : number;
+    let cost: number;
     let str1Len = str1.length;
     let str2Len = str2.length;
-    let dist : number[] = []
+    let dist: number[][] = []
 
 
-    for (i=0; i<str1Len; i++){
+    for (i = 0; i <= str1Len; i++) {
+        dist[i] = []
         dist[i][0] = i;
     }
 
-    console.log(dist)
+    for (j = 0; j <=str2Len; j++) {
+        dist[0][j] = j
+    }
+
+    for (i=1; i<=str1Len ; i ++){
+        for (j=1; j<=str2Len ; j ++){
+            if(str1.charAt(i-1) === str2.charAt(j-1)){
+                cost = 0;
+            }else{
+                cost = 1;
+            }
+            dist[i][j] = minimum(
+                dist[i-1][j]+1,
+                dist[i][j-1] +1,
+                dist[i-1][j-1] + cost
+            )
+            // console.log(`${str1.charAt(i-1)} ${str2.charAt(j-1)} ${cost}` )
+        }
+    }
+    // console.log(dist[str1Len][str2Len])
+    return dist[str1Len][str2Len]
 }
 
-levenshtein("aqsdfqsdfqsfd", "aqdfqsfdqsdfqsfd");
+levenshtein("toto", "lmpn");
