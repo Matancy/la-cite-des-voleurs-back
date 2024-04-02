43fffa6feb2c9fe811640aa99a0ab0583af65f36
blupblup46 <louan46@gmail.com>
2024-04-02 13:43:21 +0200

    intermediate

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,15 @@ import express from 'express';
 import { levenshtein } from './Levenshtein';
 import { firstLoad } from './firstLoad';
 import { getPage } from './nodeRetrieval';
+import bodyParser from 'body-parser';
 
 const app = express()
 const port = 3100
 
+firstLoad();
+
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -19,6 +24,13 @@ app.get('/pages/:page', async (req, res) => {
 app.get('/firstload', async (req, res) => {
   firstLoad();
   res.send("Database built and filled");
+});
+
+app.put('/character', (req, res) => {
+  console.log("recieved")
+  console.log(req.body);
+  res.send("ok");
+
 });
 
 
