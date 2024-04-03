bad154e4f8c24bea0ef0b233b9b0f00121085e44
blupblup46 <louan46@gmail.com>
2024-04-03 11:30:43 +0200

    fix cors

--- src/enums/cors.ts
+++ src/enums/cors.ts
@@ -3,7 +3,7 @@ export enum Method {GET="GET", POST="POST"}
 export function getCors(method : Method){
     return ({
         origin: ['http://localhost:3000', 'http://localhost:3100'],
-        methods: ['GET'],
+        methods: [`${method}`],
         allowedHeaders: ['Content-Type']
       })
 }
