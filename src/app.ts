36c237f1f34b3c8efb8c34707f27b5605062d49a
Louan Robert <louan.robert@dcbrain.com>
2024-06-03 17:31:04 +0200

    fix

--- src/app.ts
+++ src/app.ts
@@ -14,18 +14,17 @@ import { RiddleHandler } from './models/riddleHandler';
 const app = express()
 const port = 3100
 
+
 /* @here */
 app.use(bodyParser.json());
 
-/* @here */
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST");
-  res.header("Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  )
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+  res.header("Content-Type", "application/json");
   next();
-})
+});
 
 
 app.get('/images/:id', (req, res) => {
